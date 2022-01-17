2fe14ebe414430af8ae0f3b668cd0a7b392f9c9c
jack_at_dzxc <jack911jie@qq.com>
2022-01-17 16:32:34 +0800

    检查文件夹内的两种不同格式的通知

--- CheckInfo.py
+++ CheckInfo.py
@@ -11,40 +11,63 @@ class Infos:
         with open(self.fmt_file,'r',encoding='utf-8') as fn:
             self.ptns=fn.readlines()
 
-    def check_date(self):
-        fn=os.path.join(self.dir,'tz01.docx')
+    def check_date(self,fn='e:\\py\\lyy\\通知\\tz01.docx'):
 
         # ptn=re.compile('\d{2,4}年.*月.*日.*\d+(?:\:|\：)\d?.*在.*(?:召开|举行).*会议(?:\.|\,|，|。)')
+        # self.ptns=[ "r'会议时间.*'"]
+        # self.ptns=["r'\d{2,4}年.*月.*日.*\d+(?:\:|\：)\d?.*在.*(?:召开|举行).*会议(?:\.|\,|，|。)'"]
+
+        doc=Document(fn)    
+        txt=''
+        for prgh in doc.paragraphs:
+            txt=txt+prgh.text.strip()
+        txt=txt.strip().replace(' ','')
+        
+
         for pattern in self.ptns:
         # ?: 是正则括号的非捕获版本。 匹配在括号内的任何正则表达式，但该分组所匹配的子字符串 不能 在执行匹配后被获取或是之后在模式中被引用。
-            ptn=re.compile(pattern)
-            print(ptn)
-            doc=Document(fn)
-            txt=''
-            for prgh in doc.paragraphs:
-                txt=txt+prgh.text.strip()
-    
-            core_txt=ptn.findall(txt)[0]
-            ptn_date=re.compile('\d{2,4}年.*月.*日|')
-            ptn_time=re.compile('\d+(?:\:|\：)(?:\d\d|\d)')
+           
+            ptn=re.compile(eval(pattern))
+            core_txts=ptn.findall(txt)            
+            # print('core_txts',core_txts)
+            if core_txts:
+                core_txt=core_txts[0].strip().replace(' ','')
+                # print(core_txt)
+                ptn_date=re.compile('\d{2,4}年.*月.*日|')
+                ptn_time=re.compile('\d+(?:\:|\：)(?:\d\d|\d)')
 
-            meeting_date=ptn_date.findall(core_txt)[0]
-            meeting_time=ptn_time.findall(core_txt)[0]
-            
-            meet_year=re.findall('\d{2,4}年',meeting_date)[0][0:-1]
-            this_m=datetime.today().month
-            if this_m==1 or this_m==2:            
-                y=datetime.today().year
-                if int(meet_year)!=y:
-                    verify_date='yes'
+                meeting_date=list(filter(None,ptn_date.findall(core_txt)))[0].strip()
+                meeting_time=list(filter(None,ptn_time.findall(core_txt)))[0].strip()
+                meet_year=re.findall('\d{2,4}年',meeting_date)[0][0:-1]
+                this_m=datetime.today().month
+                if this_m==1 or this_m==2:            
+                    y=datetime.today().year
+                    if int(meet_year)!=y:
+                        verify_date='yes'
 
-            if core_txt:
                 break
+            
 
         return {'mt_date':meeting_date,'mt_time':meeting_time,'verify_date':verify_date}
 
+    def check_info_dir(self,dirname):
+        fns=[]
+        for fn in os.listdir(dirname):
+            if fn[-4:].lower()=='docx':
+                fns.append(os.path.join(dirname,fn))
+        for info in fns:
+            info_fn=info.split('\\')[-1]
+            print('正在检查 {} ------>>'.format(info_fn))
+            res=self.check_date(info)
+            print('结果：')
+            print('会议日期：',res['mt_date'])
+            print('会议时间：',res['mt_time'])
+            if res['verify_date']=='yes':
+                print('现在是 {} 年，请检查会议日期。'.format(datetime.today().year))
+            print('----------------------------\n')
 
 if __name__=='__main__':
     info=Infos()
-    res=info.check_date()
+    # res=info.check_date('tz01.docx')
     # print(res)
+    info.check_info_dir('e:\\py\\lyy\\通知')
